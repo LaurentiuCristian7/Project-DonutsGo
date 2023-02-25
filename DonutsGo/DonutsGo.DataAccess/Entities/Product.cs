df9932d74d08a48b218110e900098433e5e3dcfb
LaurentiuCristian7 <117767249+LaurentiuCristian7@users.noreply.github.com>
2023-02-25 17:38:48 +0200

    a
    a

--- DonutsGo/DonutsGo.DataAccess/Entities/Product.cs
+++ DonutsGo/DonutsGo.DataAccess/Entities/Product.cs
@@ -7,9 +7,13 @@ public class Product
         CreatedAt = DateTime.Now;
     }
     public Guid Id { get; set; }
+
     public string Name { get; set; }   
+
     public double Price { get; set; }
+
     public ProductType Type { get; set; }
+
 
     public DateTime CreatedAt { get; set; } 
 }

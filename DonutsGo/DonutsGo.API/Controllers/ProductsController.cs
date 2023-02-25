a6f8830d553a7fe33b07fd7b0dc4502c392d00d2
LaurentiuCristian7 <117767249+LaurentiuCristian7@users.noreply.github.com>
2023-02-25 14:32:28 +0200

    x
    x

---
+++ DonutsGo/DonutsGo.API/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using DonutsGo.API.Entities;
+using Microsoft.AspNetCore.Components;
+
+namespace DonutsGo.API.Controllers
+{
+    [Route("[controller]")]
+    public class ProductsController
+    {
+        public ProductsController()
+        { }
+
+        public List<Product> GetProducts()
+        {
+            return new List<Product>
+            {
+                new Product
+                {
+                    Id =Guid.NewGuid(),
+                    Name="Donut Chocolate",
+                    Price =10,
+                    Type =ProductType.Donut,
+
+
+                }
+            };
+        }
+    }
+}

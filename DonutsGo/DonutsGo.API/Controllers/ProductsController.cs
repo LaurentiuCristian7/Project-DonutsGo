c7c0680b2e153be02c8eba8c54ce3263eb42b358
LaurentiuCristian7 <117767249+LaurentiuCristian7@users.noreply.github.com>
2023-02-25 12:48:20 +0200

    1
    1

--- DonutsGo/DonutsGo.API/Controllers/ProductsController.cs
+++ DonutsGo/DonutsGo.API/Controllers/ProductsController.cs
@@ -27,12 +27,44 @@ namespace DonutsGo.API.Controllers
             return Storage.Products;
             
         }
+        [HttpGet("{id}")]
+        public  IActionResult GetProductById(Guid id)
+        {
+            var  product = Storage.Products.FirstOrDefault(x => x.Id==id);
+
+            return Ok(product);
+        }
+
 
         [HttpPost]
         public IActionResult CreateProduct([FromBody] Product product)
         {
             Storage.Products.Add(product);
             return Ok(product);        
+        }
+
+        [HttpPut("{id}")]
+
+        public IActionResult UpdateProduct( Guid id, [FromBody] Product newProduct)
+        {
+            var product = Storage.Products.FirstOrDefault(x => x.Id==id);
+
+            product.Name = newProduct.Name;
+            product.Price = newProduct.Price;
+            product.Type = newProduct.Type;
+
+            return Ok(product); 
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult DeleteProduct(Guid id)
+        {
+            var product = Storage.Products.FirstOrDefault(x => x.Id==id);
+
+            Storage.Products.Remove(product);
+
+            return NoContent();
         }
     }
 

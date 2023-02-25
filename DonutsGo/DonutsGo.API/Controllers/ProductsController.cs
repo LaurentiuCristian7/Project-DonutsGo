3a116c0f1d0a1ec2c65b2858e9555d31b1a8323d
LaurentiuCristian7 <117767249+LaurentiuCristian7@users.noreply.github.com>
2023-02-25 12:22:09 +0200

    1
    1

---
+++ DonutsGo/DonutsGo.API/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using DonutsGo.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace DonutsGo.API.Controllers
+{
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController()
+        { }
+
+        [HttpGet]
+        public  List<Product> GetProducts()
+        {
+          // return new List<Product>
+          //  {
+         //     new Product
+          //      {
+          //          Id = Guid.NewGuid(),
+          //          Name ="Donut Chocolate",
+          //          Price = 10,
+          //          Type = ProductType.Donut
+          //      }
+          //  };
+
+            return Storage.Products;
+            
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct([FromBody] Product product)
+        {
+            Storage.Products.Add(product);
+            return Ok(product);        
+        }
+    }
+
+
+}
---
+++ DonutsGo/DonutsGo.API/Storage.cs
@@ -0,0 +1,9 @@
+﻿using DonutsGo.API.Entities;
+
+namespace DonutsGo.API
+{
+    public static  class Storage
+    {
+        public static List<Product>Products { get; set; }  = new List<Product>();   
+    }
+}

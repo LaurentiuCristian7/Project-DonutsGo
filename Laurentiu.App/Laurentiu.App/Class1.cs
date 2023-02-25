8e390bb20b8332550a4836cbd7682569d32abc1d
LaurentiuCristian7 <117767249+LaurentiuCristian7@users.noreply.github.com>
2023-02-25 09:07:51 +0200

    1
    1

---
+++ Laurentiu.App/Laurentiu.App/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laurentiu.App
+{
+    internal class Class1
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hello World!");
+            }
+
+        }
+    }
+}  

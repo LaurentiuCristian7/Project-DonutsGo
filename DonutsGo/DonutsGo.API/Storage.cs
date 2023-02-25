c33d31914cc8b13aa7962a3aedd7a365a4e1224b
LaurentiuCristian7 <117767249+LaurentiuCristian7@users.noreply.github.com>
2023-02-25 14:01:27 +0200

    s
    s

--- DonutsGo/DonutsGo.API/Storage.cs
+++
@@ -1,9 +0,0 @@
-﻿using DonutsGo.API.Entities;
-
-namespace DonutsGo.API
-{
-    public static  class Storage
-    {
-        public static List<Product>Products { get; set; }  = new List<Product>();   
-    }
-}

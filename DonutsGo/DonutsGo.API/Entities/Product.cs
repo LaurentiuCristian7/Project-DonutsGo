816ae31d6d1255b13ed6a9c1e732612ada66860d
LaurentiuCristian7 <117767249+LaurentiuCristian7@users.noreply.github.com>
2023-02-25 11:48:38 +0200

    1
    1

---
+++ DonutsGo/DonutsGo.API/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace DonutsGo.API.Entities;
+
+public class Product
+{
+    public Guid Id { get; set; }    
+    public string Name { get; set; }
+    public double Price { get; set; }
+
+    public ProductType Type { get; set; }
+
+}
+public enum ProductType
+{
+    Donut =1,
+    Juice =2,
+    Waffles =3
+}

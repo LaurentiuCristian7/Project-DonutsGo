f084f0ad323ff7c5477d76e74a09f347ae96d209
LaurentiuCristian7 <117767249+LaurentiuCristian7@users.noreply.github.com>
2023-02-26 18:01:16 +0200

    2
    2

--- DonutsGo/DonutsGo.API/Program.cs
+++ DonutsGo/DonutsGo.API/Program.cs
@@ -1,28 +1,22 @@
 using DonutsGo.Application.Services;
-using DonutsGo.DataAccess;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IProductService,ProductService>();
 
 
-//builder.Services.AddTransient<IDataAccessDemo, DataAccessDemo>();
-//builder.Services.AddScoped<IDataAccessDemo, DataAccessDemo>();
-builder.Services.AddSingleton<IDataAccessDemo, DataAccessDemo>();
 
 
 
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();

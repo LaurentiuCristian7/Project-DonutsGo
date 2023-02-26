283083a2b05622440fb0e08239f96392a9df60d5
LaurentiuCristian7 <117767249+LaurentiuCristian7@users.noreply.github.com>
2023-02-26 18:42:33 +0200

    d
    d

---
+++ DonutsGo/DonutsGo.DataAccess/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using DonutsGo.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DonutsGo.DataAccess
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        { }
+
+        public DbSet<Product> Products { get; set; }     
+
+    }
+}
